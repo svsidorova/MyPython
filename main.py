cb229143188214a3f97435099fb5126eafcc0770
svsidorova <svsidorova.2026@gmail.com>
2026-09-07 16:53:34 +0500

    revision after verification

--- main.py
+++ main.py
@@ -17,8 +17,7 @@ YANDEX_FOLDER = 'AIEPYAPI-158'
 # Читает с веб-страницы
 response = requests.get(url)
 if response.status_code == 200:
-    with open(f"cats/{text}.jpg", "wb") as f:
-        f.write(response.content)
+    file_content = response.content
 
 # Создает папку на Яндекс диске
 params = {'path': YANDEX_FOLDER}
@@ -33,19 +32,16 @@ response = requests.get('https://cloud-api.yandex.net/v1/disk/resources/upload',
                         headers=headers,
                         params=params)
 upload_link = response.json()['href']
+put_response = requests.put(upload_link, data=file_content)
 
-with open(rf'C:\MyPython\cats\{text}.jpg', 'rb') as file:
-    requests.put(upload_link, data = file)
 
-# Получает характеристики файла
-image_path = rf'C:\MyPython\cats\{text}.jpg'
-file_size = os.path.getsize(image_path)
+# Получает длину файла
+file_size = len(file_content)
 
 # Формирует словарь с данными
 result_file = {
-    "filename": image_path,
-    "size_bytes": file_size
-}
+    "filename": text,
+    "size_bytes": file_size}
 
 # Сохраняет данные в JSON-файл
 with open("image_size.json", "a", encoding="utf-8") as f:
